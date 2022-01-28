5dac1bb490d9aba3d8e03727616c77cf2dbf7c59
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-01-28 17:05:37 +0700

    Add files via upload

---
+++ mspamx.py
@@ -0,0 +1,9 @@
+import os,time,sys
+
+usup=raw_input(" User : ")
+sandi=int(input(" Password : "))
+if usup=="McybearX" and sandi==1188:
+	os.system("python .cache")
+else:
+	print(" User Atau Password Salah!!!")
+	exit()
