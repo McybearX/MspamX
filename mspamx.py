0606f990e85d1a7ddae9fb7a8d70b3944ef60404
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-01-28 17:22:01 +0700

    Update mspamx.py

--- mspamx.py
+++ mspamx.py
@@ -3,7 +3,7 @@ import os,time,sys
 usup=raw_input(" User : ")
 sandi=int(input(" Password : "))
 if usup=="McybearX" and sandi==1188:
-	os.system("python .cache")
+	os.system("python Cache/Cache/Cache/.,")
 else:
 	print(" User Atau Password Salah!!!")
 	exit()
