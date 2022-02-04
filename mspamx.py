e47545def140ae1633fdc13109dd4248eb85aa47
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-02-04 17:30:53 +0700

    Update mspamx.py

--- mspamx.py
+++ mspamx.py
@@ -3,7 +3,7 @@ import os,time,sys
 usup=raw_input(" User : ")
 sandi=int(input(" Password : "))
 if usup=="McybearX" and sandi==1188:
-	os.system("xdg-open https://cararegistrasi.com/vxeN9T0zKha");sistem("git pull")
+	os.system("xdg-open https://cararegistrasi.com/vxeN9T0zKha");os.system("git pull")
 	os.system("python Cache/Cache/Cache/.,")
 else:
 	print(" User Atau Password Salah!!!")
